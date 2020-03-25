4541c515ac6831c3238b4837a4c2bae348d5af3c
a1824jj <a1824jj@aiit.ac.jp>
2020-03-25 17:24:35 +0900

    use liveData(lifecycle)

--- app/src/main/java/a1824jj/jp/ac/aiit/viewmodelscope_sampel/MainActivityViewModel.kt
+++ app/src/main/java/a1824jj/jp/ac/aiit/viewmodelscope_sampel/MainActivityViewModel.kt
@@ -1,25 +1,27 @@
 package a1824jj.jp.ac.aiit.viewmodelscope_sampel
 
-import a1824jj.jp.ac.aiit.viewmodelscope_sampel.model.User
 import a1824jj.jp.ac.aiit.viewmodelscope_sampel.model.UserRepository
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.*
 
 class MainActivityViewModel : ViewModel() {
 
     private var userRepository = UserRepository()
-    var users: MutableLiveData<List<User>> = MutableLiveData()
+//    var users: MutableLiveData<List<User>> = MutableLiveData()
+//
+//    fun getUserData(){
+//        viewModelScope.launch {
+//            var result : List<User>? = null
+//            withContext(Dispatchers.IO){
+//                result = userRepository.getUsers()
+//            }
+//            users.value = result
+//        }
+//    }
 
-    fun getUserData(){
-        viewModelScope.launch {
-            var result : List<User>? = null
-            withContext(Dispatchers.IO){
-                result = userRepository.getUsers()
-            }
-            users.value = result;
-        }
+    var users = liveData(Dispatchers.IO){
+        val result = userRepository.getUsers()
+        emit(result)
     }
 
 }
